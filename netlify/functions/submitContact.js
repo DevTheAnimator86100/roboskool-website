06a49c48846f54b1f820b1390d8bb3edfcdea8b5
Dev Jain <devgamerz1112@gmail.com>
2026-06-01 15:58:28 +0530

    hard coded string test

--- netlify/functions/submitContact.js
+++ netlify/functions/submitContact.js
@@ -7,7 +7,7 @@ async function connectToDatabase() {
   if (cachedClient) return cachedClient;
 
   // 1. GRAB PASSWORD AT RUNTIME (This fixes your error!)
-  const uri = process.env.ROBOSKOOL_DB_URL;
+const uri = "mongodb+srv://devgamerz1111_db_user:fqtTZI51C4tuJcyD@roboskool.waimvni.mongodb.net/?appName=RoboSkool";
 
   // 2. Failsafe just in case Netlify still can't find it
   if (!uri) {
